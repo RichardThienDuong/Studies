60bc1727054ee55ba84379054c186460d81f2829
Richard Thien Duong <richardduong827@gmail.com>
2023-08-01 22:54:58 -0700

    more leetcode

--- python/python_leetcode.py
+++ python/python_leetcode.py
@@ -798,3 +798,24 @@ class Solution: # 30mb
     
 ################################################################################################
 
+# Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+
+# You may return the answer in any order.
+
+# didnt pass , other answer 
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ## recursive function 
+        def backtrack(first = 1, curr = []) :
+
+            if len(curr) == k : 
+                output.append(curr[:])
+                return
+                
+            for i in range(first, n + 1) : 
+                curr.append(i)
+                backtrack(i + 1, curr)
+                curr.pop()
+        output= []
+        backtrack()
+        return output
