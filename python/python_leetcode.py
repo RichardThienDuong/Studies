ff4234923915d70f70ce0227515fa9b4f6734f36
Richard Thien Duong <richardduong827@gmail.com>
2023-08-02 22:33:50 -0700

    more leetcode

--- python/python_leetcode.py
+++ python/python_leetcode.py
@@ -819,3 +819,42 @@ class Solution:
         output= []
         backtrack()
         return output
+    
+###################################################################################################
+
+
+# Given an array of characters chars, compress it using the following algorithm:
+
+# Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+# If the group's length is 1, append the character to s.
+# Otherwise, append the character followed by the group's length.
+# The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+# After you are done modifying the input array, return the new length of the array.
+
+# You must write an algorithm that uses only constant extra space.
+
+## bad answer format , conclusion : 
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        d=[]
+        c=1
+        for i in range(1,len(chars)):
+            if chars[i]==chars[i-1]:
+                c+=1
+            else:
+                d.append([chars[i-1],c])
+                c=1
+        d.append([chars[-1],c]) 
+        i=0
+        for k,v in d:
+            chars[i]=k
+            i+=1
+            if v>1:
+                for item in str(v):
+                    chars[i]=str(item)
+                    i+=1
+        return i
+    
+#################################################################################################
