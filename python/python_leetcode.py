57e591edcf74a5557765a13da7a9cf6786aa6066
Richard Thien Duong <richardduong827@gmail.com>
2023-08-05 16:09:46 -0700

    more leetcode

--- python/python_leetcode.py
+++ python/python_leetcode.py
@@ -923,3 +923,31 @@ class Solution:
 
 ########################################################################################################
 
+# You are given an integer array nums and an integer k.
+
+# In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+# Return the maximum number of operations you can perform on the array.
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        sortedArr = sorted(nums)
+        left = 0
+        right = len(sortedArr) - 1
+        count = 0
+        while left < right : 
+            sumNums = sortedArr[left] + sortedArr[right]
+
+
+            if sumNums == k :
+                count += 1
+                left += 1
+                right -= 1 
+            elif sumNums < k :
+                left += 1
+            else : # sumNums > k : 
+                right -= 1
+
+        return count
+    
+################################################################################################################
