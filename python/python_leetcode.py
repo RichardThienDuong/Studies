bd08c25c4104a5707ef5509da13c639241afc577
Richard Thien Duong <richardduong827@gmail.com>
2023-08-09 14:03:32 -0700

    more leetcode

--- python/python_leetcode.py
+++ python/python_leetcode.py
@@ -1027,3 +1027,29 @@ class Solution:
         return right - left + 1  
     
 ################################################################################################################
+
+# Given a binary array nums, you should delete one element from it.
+# Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        left = 0
+        zeros = 0
+        ans = 0
+
+        for right in range(n):
+            if nums[right] == 0:
+                zeros += 1
+
+            while zeros > 1:
+                if nums[left] == 0:
+                    zeros -= 1
+                left += 1
+
+            ans = max(ans, right - left + 1 - zeros)
+
+        return ans - 1 if ans == n else ans
+    
+#####################################################################################################################
