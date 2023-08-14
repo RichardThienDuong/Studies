ce424cac3cdd22ce2ae66a3255a2fc7e504c36cf
Richard Thien Duong <richardduong827@gmail.com>
2023-08-14 16:03:40 -0700

    more leetcode

--- python/python_leetcode.py
+++ python/python_leetcode.py
@@ -1148,3 +1148,42 @@ class Solution:
     
 ###############################################################################################################
 
+# Two strings are considered close if you can attain one from the other using the following operations:
+
+# Operation 1: Swap any two existing characters.
+# For example, abcde -> aecdb
+# Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+# For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+# You can use the operations on either string as many times as necessary.
+
+# Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+
+from collections import defaultdict
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        word1D = defaultdict(int)
+        word2D = defaultdict(int)
+
+        if len(word1) != len(word2) :
+            return False
+        if ''.join(sorted(word1)) == ''.join(sorted(word2)) : 
+            return True
+        for i in range(len(word1)) : 
+            if word1[i] not in word2 or word2[i] not in word1 : 
+                return False
+            if word1D[word1[i]] :
+                word1D[word1[i]] += 1
+            else : 
+                word1D[word1[i]] = 1
+            if word2D[word2[i]] : 
+                word2D[word2[i]] += 1
+            else : 
+                word2D[word2[i]] = 1
+        if sorted(word1D.values()) == sorted(word2D.values()) : 
+            return True
+        else : 
+            return False
+
+#############################################################################################################
+
