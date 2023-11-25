371a5dd3816d7463b21668511168091be5a8f5c3
Richard Thien Duong <richardduong827@gmail.com>
2023-11-25 14:32:47 -0700

    more leetcode

--- python/python_leetcode.py
+++ python/python_leetcode.py
@@ -1321,3 +1321,28 @@ class Solution:
     
 ###########################################################################
 
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        z = 0
+        l = 0
+        if len(nums) > 1 : 
+            for i in range(len(nums)) : 
+                if nums[i] != 0 and l != i : 
+                    nums[l] = nums[i]
+                    l+=1
+                elif nums[i] != 0 : 
+                    l+=1
+                else : 
+                    z+=1
+            for i in range((len(nums) - z), len(nums)) : 
+                nums[i] = 0
+        
+###########################################################################
+
