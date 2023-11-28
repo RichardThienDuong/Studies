8754c93d1c454a7d6366671c9cbad02e7cfd5211
Richard Thien Duong <richardduong827@gmail.com>
2023-11-27 19:39:44 -0700

    more leetcode

--- python/python_leetcode.py
+++ python/python_leetcode.py
@@ -1360,3 +1360,16 @@ class Solution:
         return arrEven + arrOdd
     
 ###########################################################################
+
+# Remove Element
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
+    
+#############################################################################
