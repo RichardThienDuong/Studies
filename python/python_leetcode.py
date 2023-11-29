b11a33c0184c715aa7e00f5bd1f97e3acf449dfd
Richard Thien Duong <richardduong827@gmail.com>
2023-11-29 14:33:48 -0700

    more leetcode

--- python/python_leetcode.py
+++ python/python_leetcode.py
@@ -1373,3 +1373,23 @@ class Solution:
         return index
     
 #############################################################################
+
+# Height Checker 
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        arr = [heights[0]]
+        for i in range(1, len(heights)) : 
+                for j in range(len(arr)) : 
+                    if heights[i] <= arr[j] : 
+                        arr.insert(j, heights[i])
+                        break
+                    if j == (len(arr) - 1) : 
+                        arr.append(heights[i])
+        k = 0
+        for i in range(len(heights)) : 
+            if arr[i] != heights[i] : 
+                k+=1   
+        return k
+    
+#########################################################################
