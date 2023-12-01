cacf25eaaf1d1652c92b106a66030b3ed47ef23f
Richard Thien Duong <richardduong827@gmail.com>
2023-12-01 12:33:47 -0700

    more leetcode and Calc proj work

--- python/python_leetcode.py
+++ python/python_leetcode.py
@@ -1419,3 +1419,17 @@ class Solution:
 
 ###############################################################
 
+# Squares of a Sorted Array 
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        set_nums = set(nums)
+        arr = []
+        for i in range(1, len(nums) + 1 ) : 
+            if i not in set_nums : 
+                arr.append(i)
+        return arr
+
+########################################################################
+
+# 
