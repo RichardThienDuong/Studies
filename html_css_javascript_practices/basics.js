5b1e2ecd50d8e8dc0c5fa54d950f48ba12d09e99
Richard Duong <richardduong827@gmail.com>
2025-08-06 15:14:56 -0700

    more leetcode

--- html_css_javascript_practices/basics.js
+++ html_css_javascript_practices/basics.js
@@ -182,3 +182,17 @@ var scoreOfString = function(s) {
     return sum;
 };
 
+var differenceOfSums = function(n, m) {
+    let num1 = 0;
+    let num2 = 0;
+    
+    for (let i = 0; i < (n+1); i++) {
+        if (i % m === 0) {
+            num2 += i;
+        } else {
+            num1 += i;
+        }
+    }
+    
+    return num1 - num2;
+};
