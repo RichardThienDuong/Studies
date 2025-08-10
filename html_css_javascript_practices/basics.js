110b45c10794fdc7cfb98a1955a306de46ad0c72
Richard Duong <richardduong827@gmail.com>
2025-08-10 10:53:36 -0700

    more leetcode

--- html_css_javascript_practices/basics.js
+++ html_css_javascript_practices/basics.js
@@ -213,4 +213,7 @@ var getConcatenation = function(nums) {
     return [...nums, ...nums];
 };
 
+var convertTemperature = function(celsius) {
+    return [(celsius + 273.15), (celsius * 1.80 + 32)];
+};
 
