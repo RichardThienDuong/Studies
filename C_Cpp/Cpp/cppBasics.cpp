8656266120ca9b4be3bcffeb17d632ef7689861b
unknown <Richardduong827@gmail.com>
2026-05-10 10:41:24 -0700

    erased most of cpp studies

---
+++ C_Cpp/Cpp/cppBasics.cpp
@@ -0,0 +1,24 @@
+// #include <iostream> // Standard input/output stream
+// #include <math.h> // Mathematical functions
+#include <bits/stdc++.h> // Include all standard C++ libraries, useful in competitive programming but not recommended for production code
+using namespace std;  // so you don't have to type std:: before every standard library function
+
+// compile file first : g++ -o cppBasics cppBasics.cpp 
+// to run file : ./cppBasics.exe 
+// to compile and run in one step : g++ -o cppBasics cppBasics.cpp && ./cppBasics.exe
+
+int main() {
+    cout << "Hello, World" << "\n"; // cout is used for output
+    cout << "Hey Hisuby" << endl; // "\n" or endl for line breaks
+
+    int x;
+    cin >> x; // cin is used for input, it will prompt you to enter a value in terminal 
+    cout << "Value of x: " << x << endl;
+
+    int a, b;
+    cin >> a >> b; // example of multiple inputs : enter 10 20 
+    cout << "Value of a: " << a << " and b: " << b << endl;
+
+
+    return 0;
+}
